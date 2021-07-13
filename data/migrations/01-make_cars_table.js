1813bcd5e4881baf00e7847589183585ee61aa16
Your Name <you@example.com>
2021-07-13 16:26:09 -0400

    initial commit, added migration for table creation

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,18 @@
 exports.up = function (knex) {
   // DO YOUR MAGIC
+  return knex.schema.createTable('cars', table => {
+    table.increments();
+    table.string('vin', 128).notNullable().unique();
+    table.string('make', 128).notNullable();
+    table.string('model', 128).notNullable();
+    table.integer('mileage', 128).notNullable();
+    table.string('title', 128).nullable();
+    table.string('transmission', 128).nullable();
+  })
 };
 
 exports.down = function (knex) {
   // DO YOUR MAGIC
+  return knex.schema.dropTableIfExist('cars');
+
 };
